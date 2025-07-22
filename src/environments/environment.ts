faf420807a4d30ff37ff2b18d5b5b9f3177bed9d
ViniMatu <92049935+ViniMatu@users.noreply.github.com>
2025-07-22 01:28:25 -0300

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  aws: {
-    region: 'sa-east-1',
-    accessKeyId: 'AKIAZUKYTIGU7FI4FYAX',
-    secretAccessKey: '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'
-  }
-};
